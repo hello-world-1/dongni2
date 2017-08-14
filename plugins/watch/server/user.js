c012d8d7cf01737d233ce8d08e6a1d056da9c45f
amsfe <374801494@qq.com>
2017-08-14 02:26:53 -0700

    实现用户信息设置和用户信息获取接口

--- plugins/watch/server/user.js
+++ plugins/watch/server/user.js
@@ -3,11 +3,96 @@
  */
 const log4js = require('log4js');
 const logger = log4js.getLogger('donni');
-
 const config = appRequire('services/config').all();
 
-exports.parentDetails = (req, res) => {
-    res.send('This is not implemented now');
+const User = appRequire('plugins/watch/db/user');
+const Child = appRequire('plugins/watch/db/child');
+
+//获取用户信息
+exports.detail = (req, res) => {
+
+    user = req.body.user;
+
+    if (!user) {
+        return res.json({status: 'error', 'errcode': 3});   //用户为空
+    }
+
+    User.findOne({_id: user._id}).populate('childID').exec(function (err, user) {
+        if (err) {
+            return res.json({status: 'error', 'errcode': 3});   //数据库查询出错
+        }
+        let _user = {
+            avatar: user.avatar,
+            nickname: user.childID.nickname,
+            relationship: user.relationship,
+            childrenTelephone: user.childrenTelephone,
+            birthday: user.childID.birthday
+        };
+        console.log("user.detail:");
+        console.log(_user);
+        res.json({status: 'success', user:_user});
+    });;
+
+    // res.send('This is not implemented now');
+};
+
+//用户信息设置
+exports.modify = (req, res) => {
+
+    user = req.body.user;
+
+    nickname = req.body.nickname;
+    relationship = req.body.relationship;
+    childrenTelephone = req.body.childrenTelephone;
+    birthday = req.body.birthday;
+
+    if (!nickname || !relationship || !childrenTelephone || !birthday) {
+        return res.json({stauts: 'error', 'errcode': 3});   //有空值
+    }
+
+    //用户第一次设置
+    if (!user.childID) {
+        let _child = new Child({
+            nickname: nickname,
+            birthday: birthday,
+            parentID: user._id
+        });
+        _child.save(function (err,child) {
+            if (err) {
+                return res.json({stauts: 'error', 'errcode': 4});   //数据库保存出错
+            }
+            user.childID = child._id;
+            user.relationship = relationship;
+            user.childrenTelephone = childrenTelephone;
+            console.log("use.modify(new):");
+            console.log(user);
+            user.save(function (err) {
+                if (err) {
+                    return res.json({stauts: 'error', 'errcode': 5});   //数据库保存出错
+                }
+                res.json({statuts: 'success'});
+            })
+        })
+    }
+    //用户更新数据
+    else {
+        Child.update({_id: user.childID}, {$set: {nickname: nickname, birthday: birthday,}},function (err, child) {
+            if (err) {
+                return res.json({status: 'error', 'errcode': 6});   //数据库更新出错
+            }
+            user.relationship = relationship;
+            user.childrenTelephone = childrenTelephone;
+            console.log("user.modify(update):");
+            console.log(child);
+            console.log(user);
+            user.save(function (err) {
+                if (err) {
+                    return res.json({stauts: 'error', 'errcode': 7});   //数据库保存出错
+                }
+                res.json({statuts: 'success'});
+            })
+        })
+    }
 };
 
 exports.childrenDetails = (req, res) => {
