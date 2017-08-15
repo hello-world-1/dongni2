384f6ba1371c392141668b68f07544dd9915af2e
hello_world_1 <805209526@qq.com>
2017-08-15 03:49:16 -0700

    更新接口文档

---
+++ plugins/webgui/server/user.js
@@ -0,0 +1,90 @@
+const log4js = require('log4js');
+const logger = log4js.getLogger('donni');
+
+const config = appRequire('services/config').all();
+const Teacher = require('../db/teacher');
+const Admin = require('../db/admin');
+
+// 
+exports.login = function(req, res) {
+    // >> * username:requested
+    // >> * password:requested
+    // var _user = req.body.user
+
+    const username = req.body.username
+    const password = req.body.password
+
+    Teacher.findOne({
+        username: username
+    }, function(err, user) {
+        if (err) {
+            logger.error(err)
+            // return
+            return res.json({errcode:'500'})
+        }
+
+        if (!user) {
+            // if teacher not contain this username,query admin
+
+            Admin.findOne({
+                username: username
+            }, function(err, user) {
+                if (err) {
+                    console.log(err)
+                }
+                if (!user){
+                    // if admin not contain this username,return username not exist
+                    return res.json({errcode:'1'})
+                }else{
+                    user.comparePassword(password, function(err, isMatch) {
+                        if (err) {
+                            logger.error(err)
+                            return res.json({errcode:'500'})
+                        }
+                        if (isMatch) {
+                            // if admin match
+                            req.session.user = user
+                            req.session.type = 'admin'
+                            Admin.fetch(function(err, teachers) {
+                                return res.json({
+                                        "status": "success",
+                                        teachers:teachers
+                                        });
+                            })
+
+                        } else {
+                            return res.json({errcode:'2'});
+                        }
+                    })
+                }
+
+            })
+        }else{
+            user.comparePassword(password, function(err, isMatch) {
+                if (err) {
+                    console.log(err)
+                }
+
+                if (isMatch) {
+                    req.session.user = user
+                    req.session.type = 'admin'
+                    Admin.fetch(function(err, teachers) {
+                        return res.json({
+                            "status": "success",
+                            teachers:teachers
+                        });
+                    })
+                } else {
+                    return res.json({errcode:'2'});
+                }
+            })
+        }
+    })
+}
+
+exports.logout = function(req, res) {
+    delete req.session.user
+    delete req.session.type
+
+    return res.json({"status":"success"})
+}
---
+++ services/push.js
@@ -0,0 +1,31 @@
+var JPush = require("jpush-sdk/lib/JPush/JPush.js")
+var client = JPush.buildClient('be84b71cf631fc11f1b7d855',
+    '685ab1c6def89b5b760dc713')
+
+client.push()
+    .setPlatform(JPush.ALL) //设置推送平台,包括Android, iOS, Windows Phone
+    .setAudience(JPush.registration_id('160a3797c8300e3864c')) //设置推送目标,别名、标签、注册ID、分群、广播等方式
+    .setNotification('notification', 
+        JPush.ios('ios alert'), 
+        JPush.android('content', 'JPush Title', 1)) //设置消息发送的内容
+    .setMessage('msg content') //设置发送消息的内容
+    .setOptions(null, 60)
+    .send(function(err, res) {
+        if (err) {
+            console.log(err.message)
+        } else {
+            console.log('Sendno: ' + res.sendno)
+            console.log('Msg_id: ' + res.msg_id)
+        }
+    });
+/*client.getReportReceiveds(message_id, function(err, res) {
+    if (err) {
+        console.log(err.message)
+    } else {
+        for (var i = 0; i < res.length; i++) {
+            console.log(res[i].android_received)
+            console.log(res[i].ios_apns_sent)
+            console.log(res[i].msg_id)
+        }
+    }
+});*/
