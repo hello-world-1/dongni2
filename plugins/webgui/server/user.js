bd8175714937ce5d8d8e01ed4b7b2c125793cb9b
hello_world_1 <805209526@qq.com>
2017-08-16 03:51:45 -0700

    修改了路由及实现

--- plugins/webgui/server/user.js
+++ plugins/webgui/server/user.js
@@ -4,6 +4,8 @@ const logger = log4js.getLogger('donni');
 const config = appRequire('services/config').all();
 const Teacher = require('../db/teacher');
 const Admin = require('../db/admin');
+const Question = appRequire('plugins/watch/db/question');
+const Parent = appRequire('plugins/watch/db/user');
 
 // 
 exports.login = function(req, res) {
@@ -90,9 +92,56 @@ exports.logout = function(req, res) {
 }
 
 exports.childinfo = function(req, res) {
-    delete req.session.user
-    delete req.session.type
+    var teacherID = req.body.parentID
 
-    return res.json({"status":"success"})
+    //通过家长id获得该id下的所有提问
+    Reply.find({teacherID: user._id}).sort({createAt:-1}).exec(function (err, replys) {
+        if (err) {
+            return res.json({status: 'error', 'errcode': 2});
+        }
+        if (replys.length === 0) {
+            return res.json({status: 'error', 'errcode': 3});   //该用户没有回复
+        } else {
+            console.log("question.list:");
+            console.log(questions);
+            var replys_serialize = [];
+            replys.forEach(function (reply) {
+
+                let _teacher
+                let _question
+                let _parent
+                const content = reply.content
+
+                Teacher.findById(reply.teacherID, function(err, teacher) {
+                    if (err) {
+                        return res.json({status:'error','errcode':2});
+                    }
+                    _teacher = teacher
+                })
+
+                Question.findOne({_id:reply.questionID}, function(err, question) {
+                    if (err) {
+                        return res.json({status:'error','errcode':2});
+                    }
+                    _question = question
+                    Parent.findOne({_id:question.parentID}, function(err, parent) {
+                        if (err) {
+                            return res.json({status: 'error', 'errcode': 2});
+                        }
+                        _parent = parent
+                    })
+                })
+
+                var tmp = {
+                    parent: _parent,
+                    question: _question,
+                    teacher: _teacher,
+                    content: content
+                };
+                replys_serialize.push(tmp);
+            });
+            res.json({status: 'success', 'replys': replys_serialize});
+        }
+    });
 }
 
