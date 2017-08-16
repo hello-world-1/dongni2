19b74121dc6442742c30b66f0025d2aefdcd4d78
amsfe <374801494@qq.com>
2017-08-16 02:44:16 -0700

    新增createAt字段

--- plugins/watch/db/bill.js
+++ plugins/watch/db/bill.js
@@ -13,6 +13,14 @@ var BillSchema = new Schema({
 
     parentID: {type: Schema.Types.ObjectId, ref: 'User'},       //家长id
     lessonID: {type: Schema.Types.ObjectId, ref: 'Lesson'},     //课程id
+    createAt: {type: Date, default: Date.now}                   //订单创建时间
+});
+
+BillSchema.pre('save', function (next) {
+    if (this.isNew) {
+        this.createAt = Date.now();
+    }
+    next();
 });
 
 module.exports = mongoose.model('Bill',BillSchema);
